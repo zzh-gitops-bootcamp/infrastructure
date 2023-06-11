1f1032ad6df01895ff987e20032878ff53ec00ab
cstszhaohuazheng <samantha.zheng@cellsignal.com>
2023-06-11 19:00:12 -0400

    rollout

--- main.go
+++ main.go
@@ -12,5 +12,8 @@ func main() {
     http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "Hello, User!")
     })
+    http.HandleFunc("/friend", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, Friend!")
+    })
     http.ListenAndServe(":8080", nil)
 }
