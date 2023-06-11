d1d63ba9bec8bee858c43aa3d0071933b04c4a3a
cstszhaohuazheng <samantha.zheng@cellsignal.com>
2023-06-11 18:29:35 -0400

    CI yaml

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "Hello, World!")
     })
-
+    http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprint(w, "Hello, User!")
+    })
     http.ListenAndServe(":8080", nil)
 }
